b9e8b2d43ab11a21029477718339d9e9e5b5e7af
isaiahotico <127643977+isaiahotico@users.noreply.github.com>
2025-12-05 18:26:40 +0800

    Delete supabase.js

--- supabase.js
+++
@@ -1,6 +0,0 @@
-import { createClient } from "https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm";
-
-export const supabase = createClient(
-  "https://rhlrrpvehirrbyatphgq.supabase.co",
-  "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJobHJycHZlaGlycmJ5YXRwaGdxIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NjQ5MjQ0MTYsImV4cCI6MjA4MDUwMDQxNn0.MD0RJK-dzVihMuIhftCsDIh8Swj24gIqb6g4-DPYsw4"
-);
